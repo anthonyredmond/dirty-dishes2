cdd66b4ec5ea5dade87fd46cc9694c557729e57b
anthonyredmond <kushkurosaki@icloud.com>
2021-04-20 18:16:20 -0500

    project liftoff

---
+++ src/main/java/org/launchcode/dirtydishes/DirtyDishesApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.dirtydishes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirtyDishesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirtyDishesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/dirtydishes/DirtyDishesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.dirtydishes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirtyDishesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
