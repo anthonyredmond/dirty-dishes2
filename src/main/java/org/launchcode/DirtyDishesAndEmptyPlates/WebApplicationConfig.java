24fcab28fabde54b43d0398336afcd6a03f572d3
Jermaine Wider <JermaineWider@users.noreply.github.com>
2021-04-28 21:02:03 -0500

    Add images and media to home page

--- src/main/java/org/launchcode/DirtyDishesAndEmptyPlates/WebApplicationConfig.java
+++ src/main/java/org/launchcode/DirtyDishesAndEmptyPlates/WebApplicationConfig.java
@@ -1,5 +1,6 @@
 package org.launchcode.DirtyDishesAndEmptyPlates;
 
+import org.launchcode.DirtyDishesAndEmptyPlates.controllers.HomeController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -8,15 +9,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebApplicationConfig implements WebMvcConfigurer {
 
-    // Create spring-managed object to allow the app to access our filter
-    @Bean
-    public AuthenticationFilter authenticationFilter() {
-        return new AuthenticationFilter();
-    }
-
-    // Register the filter with the Spring container
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authenticationFilter());
-    }
 }
